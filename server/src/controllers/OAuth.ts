c3fe0a67f79141dde4a1229fed016fd6e205dab0
= <yohan.g99@gmail.com>
2024-04-23 15:39:40 +0200

    change auth system

--- server/src/controllers/OAuth.ts
+++ server/src/controllers/OAuth.ts
@@ -1,26 +1,15 @@
-import { AuthType } from '@eternal-twin/core/auth/auth-type';
-import { Url } from '@eternal-twin/core/core/url';
-import { HttpEtwinClient } from '@eternal-twin/etwin-client-http';
-import { RfcOauthClient } from '@eternal-twin/oauth-client-http/rfc-oauth-client';
 import { PrismaClient } from '@labrute/prisma';
 import { Request, Response } from 'express';
 import { ExpectedError } from '@labrute/core';
+import { OAuth2Routes, RESTPostOAuth2AccessTokenResult, RouteBases, RESTGetAPIUserResult } from 'discord.js';
 import Env from '../utils/Env.js';
 import sendError from '../utils/sendError.js';
-
-const oauthClient = new RfcOauthClient({
-  authorizationEndpoint: new Url(`${Env.ETWIN_URL}oauth/authorize`),
-  tokenEndpoint: new Url(`${Env.ETWIN_URL}oauth/token`),
-  callbackEndpoint: new Url(`${Env.SELF_URL}/oauth/callback`),
-  clientId: Env.ETWIN_CLIENT_ID,
-  clientSecret: Env.ETWIN_CLIENT_SECRET,
-});
 
 const OAuth = {
   redirect: (req: Request, res: Response) => {
     try {
       res.send({
-        url: oauthClient.getAuthorizationUri('base', ''),
+        url: `${OAuth2Routes.authorizationURL}?client_id=1232306145582911528&response_type=code&redirect_uri=${Env.SELF_URL}&scope=identify`,
       });
     } catch (error) {
       sendError(res, error);
@@ -31,42 +20,36 @@ const OAuth = {
       if (!req.query.code || typeof req.query.code !== 'string') {
         throw new ExpectedError('Invalid code');
       }
-
-      // ETwin Token
-      const token = await oauthClient.getAccessToken(req.query.code);
-
-      // ETWin User
-      const etwinClient = new HttpEtwinClient(new URL(Env.ETWIN_URL));
-      const self = await etwinClient.getAuthSelf(token.accessToken);
-
-      if (self.type !== AuthType.AccessToken) {
-        throw new Error('Invalid auth type');
-      }
-
-      // Update or store user
-      const { user: etwinUser } = self;
+      const data_1 = new URLSearchParams();
+      data_1.append('client_id', Env.DISCORD_CLIENT_ID);
+      data_1.append('client_secret', Env.DISCORD_CLIENT_SECRET);
+      data_1.append('grant_type', 'authorization_code');
+      data_1.append('redirect_uri', Env.SELF_URL);
+      data_1.append('scope', 'identify');
+      data_1.append('code', req.query.code);
+      const resToken: RESTPostOAuth2AccessTokenResult = await fetch(OAuth2Routes.tokenURL, { method: 'POST', body: data_1 }).then((res) => res.json());
+      const discordUser: RESTGetAPIUserResult = await fetch(`${RouteBases.api}/users/@me`, { headers: { authorization: `Bearer ${resToken.access_token}` } }).then((res) => res.json());
 
       const existingUser = await prisma.user.findFirst({
-        where: { id: etwinUser.id },
+        where: { id: discordUser.id },
       });
 
       // If user does not exist, create it
       if (!existingUser) {
         await prisma.user.create({
           data: {
-            id: etwinUser.id,
-            connexionToken: token.accessToken,
-            name: etwinUser.displayName.current.value,
+            id: discordUser.id,
+            connexionToken: resToken.access_token,
+            name: discordUser.username,
           },
           select: { id: true },
         });
       } else {
         // If user exists, update it
         await prisma.user.update({
-          where: { id: etwinUser.id },
+          where: { id: discordUser.id },
           data: {
-            name: etwinUser.displayName.current.value,
-            connexionToken: token.accessToken,
+            connexionToken: resToken.access_token,
           },
           select: { id: true },
         });
@@ -74,7 +57,7 @@ const OAuth = {
 
       // Fetch user data
       const user = await prisma.user.findFirst({
-        where: { id: etwinUser.id, connexionToken: token.accessToken },
+        where: { id: discordUser.id, connexionToken: resToken.access_token },
         include: {
           brutes: {
             where: { deletedAt: null },
